68496ba4564ed3749b977f273d22639228782988
JuanArbeU <juan.arbelaez514@gmail.com>
2023-09-04 19:38:15 -0500

    third commit

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { saveDiets } = require('./src/controllers/getDietsDb.js')
+const port = process.env.PORT || 3000;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async() => {
   //await saveDiets()
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
